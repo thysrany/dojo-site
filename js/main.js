845ef0a847dd4567bda860aa5cfa4c08851aab18
Rany Thys <thysrany@gmail.com>
2026-09-13 21:34:40 -0400

    Faire glisser les kanji en filigrane depuis les cotes

--- js/main.js
+++ js/main.js
@@ -27,7 +27,7 @@
   }
 
   // ---- Scroll reveal (block-level, staggered groups, and the enso draw-on) ----
-  var revealEls = document.querySelectorAll(".reveal, .reveal-stagger, .enso");
+  var revealEls = document.querySelectorAll(".reveal, .reveal-stagger, .enso, .kanji-watermark");
   if ("IntersectionObserver" in window && revealEls.length) {
     var io = new IntersectionObserver(
       function (entries) {
